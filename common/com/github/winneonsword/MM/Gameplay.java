7ec9c0a4e0bea27f273e2bacad15e43dd64eca39
WinneonSword <winneonsword@gmail.com>
2013-11-10 22:02:47 -0600

    Added the end game sequence

--- common/com/github/winneonsword/MM/Gameplay.java
+++ common/com/github/winneonsword/MM/Gameplay.java
@@ -5,8 +5,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import com.github.winneonsword.MM.utils.UtilsGameplay;
+import com.github.winneonsword.MM.utils.UtilsMM;
 
 public class Gameplay extends UtilsGameplay implements Listener {
+	
+	private String[] welcome;
+	private int runWelcome;
+	private int welcomeSlide;
 	
 	public Gameplay(MainMM pl){
 		
@@ -29,6 +34,98 @@ public class Gameplay extends UtilsGameplay implements Listener {
 		}
 		
 		this.welcomePlayers();
+		
+	}
+	
+	public void endGame(){
+		
+		for (int i = 0; i < this.getPlayerList().size(); i++){
+			
+			String pl = this.getPlayerList().get(i);
+			Player p = Bukkit.getPlayer(pl);
+			final Player finalP = p;
+			
+			this.setVariables(p);
+			this.clearInven();
+			
+			this.sMM("Mob Mondays has ended! Thank you for playing, warping you to spawn.");
+			
+			this.delay(this.pl, new Runnable(){
+				
+				public void run(){
+					
+					finalP.performCommand("s");
+					
+				}
+				
+			}, 40L);
+			
+		}
+		
+	}
+	
+	public void beginRound(int round){
+		
+		this.setRound(round);
+		
+		switch (round){
+		
+		default:
+			
+			this.sMM("An unknown round has started! Stopping MM.");
+			this.endGame();
+			break;
+			
+		}
+		
+	}
+	
+	public void welcomePlayers(){
+		
+		this.welcome = UtilsMM.AS(new String[] {
+				
+				"Welcome to Mob Mondays!",
+				"Please read carefully for the rules!",
+				"No leaving the arena via any command except /mm leave!",
+				"If you find a glitch, please report it to &7Winneon&d!",
+				"Do &c&lNOT &dask for heals or feeds! These get distributed automatically every round!",
+				"All MM related commands are located under /mm ? or /mm help!",
+				"The player list is located under /mm list!",
+				"To view your stats such as shards, mob kills, etc, type /mm stats or look at the sidebar!",
+				"To use your alpha ability, right click the green music disc!",
+				"To use your omega ability, right click the gold music disc!",
+				"Remember, abilities use shards as a cost! The prices are listed /mm class <class>!",
+				"To deposit the shards, you collect, just right click them!",
+				"&c&lWARNING: &cMM will be difficult. Work together with your teammates to stay alive.",
+				"Alright! Here we go! I'll make it easy for you the first round."
+				
+		});
+		
+		this.welcomeSlide = 0;
+		
+		this.runWelcome = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this.pl, new Runnable(){
+			
+			public void run(){
+				
+				checkWelcome();
+				
+				UtilsMM.sMM(welcome[welcomeSlide]);
+				welcomeSlide++;
+				
+			}
+			
+		}, 0L, 60L);
+		
+	}
+	
+	private void checkWelcome(){
+		
+		if (this.welcomeSlide >= this.welcome.length){
+			
+			Bukkit.getServer().getScheduler().cancelTask(this.runWelcome);
+			this.beginRound(1);
+			
+		}
 		
 	}
 	
--- common/com/github/winneonsword/MM/utils/UtilsGameplay.java
+++ common/com/github/winneonsword/MM/utils/UtilsGameplay.java
@@ -1,6 +1,5 @@
 package com.github.winneonsword.MM.utils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -16,9 +15,7 @@ public class UtilsGameplay extends UtilsMM {
 	private String clazz;
 	private ClassData data = null;
 	
-	private String[] welcome;
-	private int runWelcome;
-	private int welcomeSlide;
+	private int round;
 	
 	public UtilsGameplay(MainMM pl){
 		
@@ -26,11 +23,29 @@ public class UtilsGameplay extends UtilsMM {
 		
 	}
 	
+	public int getRound(){
+		
+		return this.round;
+		
+	}
+	
+	public void setRound(int round){
+		
+		this.round = round;
+		
+	}
+	
 	public void giveGameItems(){
 		
-		this.inven.clear();
+		this.clearInven();
 		this.giveClassItems();
 		this.giveStandardItems();
+		
+	}
+	
+	public void clearInven(){
+		
+		this.inven.clear();
 		
 	}
 	
@@ -51,54 +66,6 @@ public class UtilsGameplay extends UtilsMM {
 		
 		this.inven.setItem(7, data.getAlphaDisc());
 		this.inven.setItem(8, data.getOmegaDisc());
-		
-	}
-	
-	public void welcomePlayers(){
-		
-		this.welcome = UtilsMM.AS(new String[] {
-				
-				"Welcome to Mob Mondays!",
-				"Please read carefully for the rules!",
-				"No leaving the arena via any command except /mm leave!",
-				"If you find a glitch, please report it to &7Winneon&d!",
-				"Do &c&lNOT &dask for heals or feeds! These get distributed automatically every round!",
-				"All MM related commands are located under /mm ? or /mm help!",
-				"The player list is located under /mm list!",
-				"To view your stats such as shards, mob kills, etc, type /mm stats or look at the sidebar!",
-				"To use your alpha ability, right click the green music disc!",
-				"To use your omega ability, right click the gold music disc!",
-				"Remember, abilities use shards as a cost! The prices are listed /mm class <class>!",
-				"To deposit the shards, you collect, just right click them!",
-				"&c&lWARNING: &cMM will be difficult. Work together with your teammates to stay alive.",
-				"Alright! Here we go!"
-				
-		});
-		
-		this.welcomeSlide = 0;
-		
-		this.runWelcome = Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this.pl, new Runnable(){
-			
-			public void run(){
-				
-				checkWelcome();
-				
-				UtilsMM.sMM(welcome[welcomeSlide]);
-				welcomeSlide++;
-				
-			}
-			
-		}, 0L, 60L);
-		
-	}
-	
-	private void checkWelcome(){
-		
-		if (this.welcomeSlide >= this.welcome.length){
-			
-			Bukkit.getServer().getScheduler().cancelTask(this.runWelcome);
-			
-		}
 		
 	}
 	
